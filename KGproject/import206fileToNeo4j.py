eccaebb5cbbfb53999b7a14472584c9214e9dd29
jia.baojing <jia.baojing@bendis.cc>
2021-03-11 17:00:39 +0800

    修改代码，添加文档

--- KGproject/import206fileToNeo4j.py
+++ KGproject/import206fileToNeo4j.py
@@ -3,50 +3,43 @@ from py2neo import Graph, Node, Relationship, NodeMatcher
 import xlrd
 import os
 import math
-import re
-import openpyxl
+
 from globalFunction import connectNeo4j,wipe_line_break,extractRelation,getpartdic,get_allfile
 partdic={}
 reldic={}
 entitydic={}
 equiparray=[]
-# def getpartdic():
-def saveFilecontentToNeo4j(filepath):
+detctionarray=[]
 
-    global document,documentnode,entitydic,reldic,relnum,entitynum,valueType,partdic,filename,provalueType,equiparray,filedata,equipname,filename
+def saveFilecontentToNeo4j(filepath,fileType):
+
+    global document,documentnode,entitydic,reldic,relnum,entitynum,valueType,partdic,filename,provalueType,equiparray,filedata,equipname
     reldic.clear()
     entitydic.clear()
     filedata = xlrd.open_workbook(filepath)
-    filetable = filedata.sheet_by_index(0)
-    matcher = NodeMatcher(file_graph)
+    filetable1 = filedata.sheet_by_index(0)
+    filetable2 = filedata.sheet_by_index(1)
+
     relnum = 0
     entitynum = 0
     filename=os.path.basename(filepath).replace(" ","").replace(".xlsx","")
-    if "检修" in filename:
-        fileType="service"
-    if "评价" in filename or "策略" in filename:
-        fileType="evaluate"
-    if "验收" in filename:
-        fileType="acceptance"
-    if "运维" in filename:
-        fileType="operations"
 
-    equipname=filetable.cell_value(1, 0)
+    equipname=filetable1.cell_value(1, 0)
 
     for key in partdic.keys():
         if key not in equiparray:
             equiparray.append(key)
 
-    for row in range(1,filetable.nrows):
-        for col in range(0,filetable.ncols):
+    for row in range(1,filetable1.nrows):
+        for col in range(0,filetable1.ncols):
             # print(str(row),"  ",str(col))
-            value = filetable.cell_value(row, col)
+            value = filetable1.cell_value(row, col)
             if type(value) == str:
                 value = wipe_line_break(value)
             if value == "":
                 continue
 
-            attribute =  filetable.cell_value(0, col)
+            attribute =  filetable1.cell_value(0, col)
             attribute = wipe_line_break(attribute)
             if attribute=="实体":
                 name = ""
@@ -54,13 +47,7 @@ def saveFilecontentToNeo4j(filepath):
                 relid = int(col / 2)
                 entityid = math.ceil(col / 2)
                 #判断实体类型
-                valueType = ""
-                if value in partdic[equipname]:
-                    valueType = "part"
-                if value in equiparray:
-                    valueType = "equip"
-
-
+                valueType = getEntityType(value)
                 if len(valueType)==0:
                     valueType = fileType
 
@@ -69,12 +56,7 @@ def saveFilecontentToNeo4j(filepath):
                         fathernode=entitydic[entityid-1]
                         relation=reldic[relid-1]
                         if relation!="" and fathernode!="":
-
-                            if valueType=="detail":
-                                name="详情"
-                                desc=value
-                            else:
-                                name=value
+                            name=value
                         newnode = matcher.match(valueType, name=value, desc=desc,equip=equipname).first()
                         if newnode is None:
                             newnode = Node(valueType, name=name, desc=desc,equip=equipname)
@@ -84,49 +66,32 @@ def saveFilecontentToNeo4j(filepath):
                     elif col==0:
                         reldic.clear()
                         entitydic.clear()
-                        newnode = matcher.match("equip", name=value).first()
+                        newnode = matcher.match(valueType, name=value).first()
                         if (newnode is None):
-                            newnode = Node("equip", name=value)
+                            newnode = Node(valueType, name=value)
                             file_graph.create(newnode)
                         entitydic[entityid] = newnode
                 elif len(reldic)==0 and len(entitydic)==0:
-                    newnode = matcher.match("equip", name=value).first()
+                    newnode = matcher.match(valueType, name=value).first()
                     if (newnode is None):
-                        newnode = Node("equip", name=value)
+                        newnode = Node(valueType, name=value)
                         file_graph.create(newnode)
                     entitydic[entityid]=newnode
             elif attribute=="关系":
                 reldic[int(col/2)]=value
-    savefileKnowledgePoint(filepath)
+    savefileKnowledgePoint(filetable2,fileType)
 
-def savefileKnowledgePoint(filepath):
+def savefileKnowledgePoint(filetable2,fileType):
 
-    filetable = filedata.sheet_by_index(1)
-    matcher = NodeMatcher(file_graph)
-
-    if "检修" in filename:
-        provalueType = "service"
-    if "评价" in filename:
-        provalueType = "evaluate"
-    if "验收" in filename:
-        provalueType = "acceptance"
-    if "运维" in filename:
-        provalueType = "operations"
-
-    for key in partdic.keys():
-        if key not in equiparray:
-            equiparray.append(key)
-
-    for row in range(1, filetable.nrows):
-        for col in range(0, filetable.ncols):
+    for row in range(1, filetable2.nrows):
+        for col in range(0, filetable2.ncols):
             # print(str(row),"  ",str(col))
-            value = filetable.cell_value(row, col)
+            value = filetable2.cell_value(row, col)
             if type(value) == str:
                 value = wipe_line_break(value)
             if value == "":
                 continue
-
-            attribute = filetable.cell_value(0, col)
+            attribute = filetable2.cell_value(0, col)
             attribute = wipe_line_break(attribute)
             if attribute == "实体":
                 name = ""
@@ -134,16 +99,9 @@ def savefileKnowledgePoint(filepath):
                 relid = int(col / 2)
                 entityid = math.ceil(col / 2)
                 # 判断实体类型
-                valueType = ""
-                if value in partdic[equipname]:
-                    valueType = "part"
-                if value in equiparray:
-                    valueType = "equip"
-                if (len(valueType) == 0 and len(value) < 20) or provalueType == "evaluate":
-                    valueType = provalueType
-
+                valueType = getEntityType(value)
                 if len(valueType) == 0:
-                    valueType = "detail"
+                    valueType = fileType
 
                 if len(reldic) > 0 and len(entitydic) > 0:
                     if col > 0:
@@ -154,11 +112,6 @@ def savefileKnowledgePoint(filepath):
                                 valueType="drafter"
                             elif relation=="起草单位":
                                 valueType = "department"
-                            if valueType == "detail":
-                                name = "详情"
-                                desc = value
-                            else:
-                                name = value
                         newnode = matcher.match(valueType, name=value, desc=desc, equip=equipname).first()
                         if newnode is None:
                             newnode = Node(valueType, name=name, desc=desc, equip=equipname)
@@ -182,20 +135,46 @@ def savefileKnowledgePoint(filepath):
             elif attribute == "关系":
                 reldic[int(col / 2)] = value
 
+  # 判断实体类型是否为设备或部件
+def getEntityType(entity):
+    if entity in partdic[equipname]:
+        entityType = "part"
+    if entity in equiparray:
+        entityType = "equip"
+    return entityType
+
 
 if __name__ == "__main__":
-    global file_graph
+    global file_graph,matcher
     file_graph = connectNeo4j()
+    matcher = NodeMatcher(file_graph)
+    partdic=getpartdic(r".\文档\equip.xlsx")
 
-    partdic=getpartdic(r"D:\知识图谱\油浸式变压器\equip.xlsx")
+    path = r".\文档"
 
-    transformerpath = r"D:\知识图谱\文档"
-    transformerarray = []
-    # transformerarray.append(transformerpath + "\国家电网公司变电评价管理规定（试行） 第1分册 油浸式变压器（电抗器）精益化评价细则.xlsx")
-    transformerarray.append(transformerpath + "\国家电网公司变电检修管理规定（试行） 第1分册 油浸式变压器（电抗器）检修细则.xlsx")
-    # transformerarray.append(transformerpath + "\国家电网公司变电验收管理规定（试行） 第1分册  油浸式变压器（电抗器）验收细则.xlsx")
-    # transformerarray.append(transformerpath + "\国家电网公司变电运维管理规定（试行） 第1分册  油浸式变压器（电抗器）运维细则.xlsx")
-    #
-    for path in transformerarray:
-        saveFilecontentToNeo4j(path)
+    servicefilearray=[]
+    evaluatefilearray=[]
+    acceptancefilearray=[]
+    operationsfilearray=[]
+    detectionfilearray=[]
+
+
+    detectionfilearray=get_allfile(path + "\变电检测管理规定细则")
+    servicefilearray=get_allfile(path + "\变电检修管理规定细则")
+    operationsfilearray= get_allfile(path + "\变电运维管理规定细则")
+    acceptancefilearray=get_allfile(path + "\变电验收管理规定细则")
+    evaluatefilearray=get_allfile(path + "\变电评价管理规定细则")
+
+
+    for file in detectionfilearray:
+        saveFilecontentToNeo4j(file, "检测")
+    for file in servicefilearray:
+        saveFilecontentToNeo4j(file, "检修")
+    for file in operationsfilearray:
+        saveFilecontentToNeo4j(file, "运维")
+    for file in acceptancefilearray:
+        saveFilecontentToNeo4j(file,"验收")
+    for file in evaluatefilearray:
+        saveFilecontentToNeo4j(file,"评价")
+
 
